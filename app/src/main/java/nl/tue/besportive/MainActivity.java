7c9e32525eb3d9e15450c2af4c5507b9a3d95b82
Minkeh <92096994+Minkeh@users.noreply.github.com>
2023-03-19 23:11:00 +0100

    Almost finished feed page

--- app/src/main/java/nl/tue/besportive/MainActivity.java
+++ app/src/main/java/nl/tue/besportive/MainActivity.java
@@ -8,8 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
