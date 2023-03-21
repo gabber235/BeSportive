6efb10830267df7c67c17fc1eb8cbd72f5ea273f
Gabber235 <gabber200@icloud.com>
2023-03-21 12:49:12 +0100

    Create base functions

--- app/src/main/java/nl/tue/besportive/CreateGroupModel.java
+++ app/src/main/java/nl/tue/besportive/CreateGroupModel.java
@@ -1,9 +1,16 @@
 package nl.tue.besportive;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.functions.FirebaseFunctions;
+
+import java.util.concurrent.TimeUnit;
 
 public class CreateGroupModel extends BaseObservable {
     private String name;
@@ -32,5 +39,19 @@ public class CreateGroupModel extends BaseObservable {
 
     public void createGroup(Context context) {
         setLoading(true);
+        FirebaseFunctions.getInstance().getHttpsCallable("createGroup")
+                .withTimeout(10, TimeUnit.SECONDS)
+                .call(name)
+                .addOnCompleteListener(task -> onGroupCreated(context, task));
+    }
+
+    private void onGroupCreated(Context context, Task task) {
+        if (task.isSuccessful()) {
+            Navigator.navigateToInviteMembersActivity(context);
+        } else {
+            setLoading(false);
+            Log.e("CreateGroupModel", "Failed to create group", task.getException());
+            Toast.makeText(context, "Failed to create group", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +8,83 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const onUserCreate = functions.auth.user().onCreate(async (user) => {
+  const userRef = admin.firestore().collection("users").doc(user.uid);
+  await userRef.set({
+    email: user.email,
+    name: user.displayName,
+    photoUrl: user.photoURL,
+    totalTime: 0,
+    totalChallenges: 0,
+  });
+});
+
+export const onUserDelete = functions.auth.user().onDelete(async (user) => {
+  const userRef = admin.firestore().collection("users").doc(user.uid);
+  await userRef.delete();
+});
+
+export const createGroup = functions.https.onCall(async (data, context) => {
+  const name = data;
+  const auth = context.auth;
+  if (!auth) {
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "User must be authenticated to create a group"
+    );
+  }
+  const {uid} = auth;
+  const user = await admin.auth().getUser(uid);
+
+  // Check if user is already in a group
+  if (await isInGroup(uid)) {
+    throw new functions.https.HttpsError(
+      "already-exists",
+      "User is already in a group"
+    );
+  }
+
+  const groupRef = admin.firestore().collection("groups").doc();
+  await groupRef.set({
+    name,
+    creationDate: admin.firestore.FieldValue.serverTimestamp(),
+    admin: uid,
+    members: {
+      [uid]: {
+        name: user.displayName,
+        photoUrl: user.photoURL,
+      },
+    },
+    code: generateGroupCode(),
+  });
+
+  return groupRef.id;
+});
+
+/**
+ * Generates a random 5 character group code.
+ * @return {string} The group code.
+ */
+function generateGroupCode() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let code = "";
+  for (let i = 0; i < 5; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return code;
+}
+
+/**
+ * Checks if a user is already in a group.
+ * @param {string} uid The user's uid.
+ * @return {Promise<boolean>} True if the user is in a group, false otherwise.
+ */
+async function isInGroup(uid: string) {
+  const existingGroup = await admin.firestore().collection("groups")
+    .orderBy(`members.${uid}`)
+    .limit(1)
+    .get();
+
+  return !existingGroup.empty;
+}
