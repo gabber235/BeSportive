570405e14b86a7c777f64e5996586c87a5b64ed9
Minkeh <92096994+Minkeh@users.noreply.github.com>
2023-03-23 12:16:57 +0100

    updated toolbar

--- app/src/main/java/nl/tue/besportive/FeedActivity.java
+++ app/src/main/java/nl/tue/besportive/FeedActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,8 +22,26 @@ public class FeedActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
         binding = ActivityFeedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FeedActivity.this, "You clicked on the left icon",  Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rightIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMenu(v);
+            }
+        });
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
@@ -47,7 +69,23 @@ public class FeedActivity extends AppCompatActivity {
                 }
                 return false;
             }
+
         });
     }
 
-}
+    private void showMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(FeedActivity.this,v);
+        popupMenu.getMenuInflater().inflate(R.menu.toolbar_settings_menu,popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()) {
+            @Override
+            public boolean onMenuItemClick (MenuItem item){
+                if(item.getItemId() == R.id.configure_challenges)
+                    if(item.getItemId() == R.id.create_group)
+                        if(item.getItemId() == R.id.leave_group)
+                return true;
+            }
+        }
+
+        popupMenu.show();
+        }
+    }
