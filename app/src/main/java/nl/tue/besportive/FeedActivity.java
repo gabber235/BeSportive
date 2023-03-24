06271e6dc8df2e5829cf1d271c1d274dd9560808
Minkeh <92096994+Minkeh@users.noreply.github.com>
2023-03-24 11:46:14 +0100

    change document

--- app/src/main/java/nl/tue/besportive/FeedActivity.java
+++ app/src/main/java/nl/tue/besportive/FeedActivity.java
@@ -32,34 +32,36 @@ public class FeedActivity extends AppCompatActivity {
         binding = ActivityFeedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.feed);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.challenges:
+                        startActivity(new Intent(getApplicationContext(),ChallengesActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.feed:
+                        return true;
+                    case R.id.leaderboard:
+                        startActivity(new Intent(getApplicationContext(),LeaderboardActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        
 
 
     }
-    // Initialize and assign variable
-    BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
-
-    // Set Home selected
-            bottomNavigationView.setSelectedItemId(R.id.feed);
-
-    // Perform item selected listener
-            bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(MenuItem item) {
-            switch(item.getItemId()) {
-                case R.id.challenges:
-                    startActivity(new Intent(getApplicationContext(),ChallengesActivity.class));
-                    overridePendingTransition(0,0);
-                    return true;
-                case R.id.feed:
-                    return true;
-                case R.id.leaderboard:
-                    startActivity(new Intent(getApplicationContext(),LeaderboardActivity.class));
-                    overridePendingTransition(0,0);
-                    return true;
-            }
-            return false;
-        }
-    });
 
 
         @Override
