aedb2a305048a21fe6e88f76da9d89668d08c1a1
Minkeh <92096994+Minkeh@users.noreply.github.com>
2023-03-24 10:39:46 +0100

    continuation of toolbar and feed

--- app/src/main/java/nl/tue/besportive/FeedActivity.java
+++ app/src/main/java/nl/tue/besportive/FeedActivity.java
@@ -2,10 +2,12 @@ package nl.tue.besportive;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.app.Activity;
+import android.view.Menu;
 import android.widget.PopupMenu;
 import android.content.Intent;
 import android.os.Bundle;
@@ -30,102 +32,84 @@ public class FeedActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // assigning ID of the toolbar to a variable
-    //    Toolbar toolbar = findViewById(R.id.toolbar);
+        //    Toolbar toolbar = findViewById(R.id.toolbar);
 
         // using toolbar as ActionBar
         //  setSupportActionBar(toolbar);
 
         // Initialize and assign variable
-      //  AppBarLayout appBarLayout=findViewById(R.id.settings_menu);
+        //  AppBarLayout appBarLayout=findViewById(R.id.settings_menu);
 
         // Set Home selected
- //       appBarLayout.setSelectedItemId(R.id.feed);
+        //       appBarLayout.setSelectedItemId(R.id.feed);
 
         // Perform item selected listener
-   //     appBarLayout.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener()
+        //     appBarLayout.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener()
 
-   //     @Override
-    //    public boolean onCreateOptionsMenu(Menu menu) {
-  //          MenuInflater inflater = getMenuInflater();
-   //         inflater.inflate(R.menu.settings_menu, menu);
-   //         return true;
+        //     @Override
+        //    public boolean onCreateOptionsMenu(Menu menu) {
+        //          MenuInflater inflater = getMenuInflater();
+        //         inflater.inflate(R.menu.settings_menu, menu);
+        //         return true;
 
-   //     }
+    }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu){
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.settings_menu, menu);
-            return true;
-        }
-
-
-
-
-     //       switch(item.getItemId()) {
-     //           case R.id.create_group:
-      //              startActivity(new Intent(getApplicationContext(),CreateGroupActivity.class));
-      //              overridePendingTransition(0,0);
-        //return true;
-      //          case R.id.configure_challenges:
-      //              startActivity(new Intent(getApplicationContext(),ConfigureChallengesActivity.class));
-      //              overridePendingTransition(0,0);
-      //              return true;
-      //          case R.id.profile_button:
-      //              startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-      //              overridePendingTransition(0,0);
-       //             return true;
-       //     }
-      //      return false;
-      //  }
-
-
-
-      //  @Override
-      //  public boolean onCreateOptionsMenu(Menu menu) {
-    //        MenuInflater inflater = getMenuInflater();
-    //        inflater.inflate(R.menu.settings_menu, menu);
-    //        return true;
-    //    }
-
-     //   Override
-     //   public boolean onOptionsItemSelected(MenuItem item) {
-
-     //       switch (item.getItemId()) {
-     //           case R.id.profile_button:
-     //               startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-     //               overridePendingTransition(0,0);
-     //               return true;
-     //       }
-     //       return false;
-     //   }
-
-        // Initialize and assign variable
-        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
-
-        // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.feed);
-
-        // Perform item selected listener
-        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
+            public boolean onCreateOptionsMenu(Menu menu){
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.settings_menu, menu);
+                return true;
+            }
 
-                switch(item.getItemId())
-                {
-                    case R.id.challenges:
-                        startActivity(new Intent(getApplicationContext(),ChallengesActivity.class));
-                        overridePendingTransition(0,0);
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.profile_button:
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
-                    case R.id.feed:
+                    case R.id.create_group:
+                        startActivity(new Intent(getApplicationContext(), CreateGroupActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
-                    case R.id.leaderboard:
-                        startActivity(new Intent(getApplicationContext(),LeaderboardActivity.class));
-                        overridePendingTransition(0,0);
+                    case R.id.configure_challenges:
+                        startActivity(new Intent(getApplicationContext(), ConfigureChallengesActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
-            }
+
+
+
+
+
+
+
+
+        // Initialize and assign variable
+            BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+            bottomNavigationView.setSelectedItemId(R.id.feed);
+
+        // Perform item selected listener
+            bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    switch(item.getItemId()) {
+                        case R.id.challenges:
+                            startActivity(new Intent(getApplicationContext(),ChallengesActivity.class));
+                            overridePendingTransition(0,0);
+                        return true;
+                        case R.id.feed:
+                            return true;
+                        case R.id.leaderboard:
+                            startActivity(new Intent(getApplicationContext(),LeaderboardActivity.class));
+                            overridePendingTransition(0,0);
+                            return true;
+                    }
+                    return false;
+                }
         });
     }
 
