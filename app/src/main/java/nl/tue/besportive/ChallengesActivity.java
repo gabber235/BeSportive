a5873b16f5a79f73aa5bd49222eafc364434a747
fore1gner <emrealtantr@gmail.com>
2023-03-24 02:26:15 +0100

    Creating Adapters-Challenges Page
    Create Adapters for the challenges page is done, data is ready. Next i have fetch the challenges for the challenges page. I have all ready. I will implement the Firebase next to finish it

--- app/src/main/java/nl/tue/besportive/ChallengesActivity.java
+++ app/src/main/java/nl/tue/besportive/ChallengesActivity.java
@@ -1,23 +1,66 @@
 package nl.tue.besportive;
 
 import android.content.Intent;
+import android.hardware.lights.LightState;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import nl.tue.besportive.databinding.ActivityChallengesBinding;
+import okhttp3.Challenge;
 
 public class ChallengesActivity extends AppCompatActivity {
 
     private ActivityChallengesBinding binding;
+    List<Challenges> challengesList = new ArrayList<Challenges>();
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
+    private static final String TAG = "Challenges App";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityChallengesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        fillChallengesList();
+        Log.d(TAG, "onCreate:"+ challengesList.toString());
+        Toast.makeText(this,"Challenges Count =" + challengesList.size(), Toast.LENGTH_LONG).show();
+
+        RecyclerView recyclerView = findViewById(R.id.ly_challengeslist);
+        if (recyclerView != null) {
+            recyclerView.setHasFixedSize(true);
+        }
+        recyclerView =  findViewById(R.id.ly_challengeslist);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        mAdapter = new RecycleViewAdapter(challengesList);
+        recyclerView.setAdapter(mAdapter);
     }
+
+    private void fillChallengesList() {
+        Challenges c1  = new Challenges(0, "Run 5 km");
+        Challenges c2  = new Challenges(1, "Run 10km");
+        Challenges c3  = new Challenges(2, "100 push ups");
+        challengesList.addAll(Arrays.asList( new Challenges[]{c1,c2,c3}));
+    }
+
+
+
     private void leaderboard(View view) {
         startLeaderboardActivity();
     }
