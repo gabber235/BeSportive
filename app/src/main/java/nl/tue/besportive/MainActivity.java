0760079035aa279b0f7d502984d62627b5094eca
Gabber235 <gabber200@icloud.com>
2023-04-03 08:20:12 +0200

    Extract starting page to navigator

--- app/src/main/java/nl/tue/besportive/MainActivity.java
+++ app/src/main/java/nl/tue/besportive/MainActivity.java
@@ -4,15 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.functions.FirebaseFunctions;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,39 +35,6 @@ public class MainActivity extends AppCompatActivity {
             FirebaseFirestore.getInstance().setFirestoreSettings(productionSettings);
         }
 
-        checkIfUserIsStillLoggedIn();
-    }
-
-    private void checkIfUserIsStillLoggedIn() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user == null) {
-            Navigator.navigateToOnboardingActivity(this, true);
-            return;
-        }
-
-        user.reload().addOnCompleteListener(this::onUserReloaded);
-    }
-
-    private void onUserReloaded(Task<Void> task) {
-        if (task.isSuccessful()) {
-            checkIfUserHasGroup(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()));
-        } else {
-            Navigator.navigateToOnboardingActivity(this, true);
-        }
-    }
-
-    private void checkIfUserHasGroup(FirebaseUser user) {
-        FirebaseFirestore.getInstance().collection("groups")
-                .whereNotEqualTo("members." + user.getUid(), null)
-                .get()
-                .addOnCompleteListener(this::onGroupQueryComplete);
-    }
-
-    private void onGroupQueryComplete(Task<QuerySnapshot> task) {
-        if (task.isSuccessful() && !task.getResult().isEmpty()) {
-            Navigator.navigateToFeedActivity(this, true);
-        } else {
-            Navigator.navigateToJoinCreateGroupActivity(this, true);
-        }
+        Navigator.navigateToStartingPage(this);
     }
 }
--- app/src/main/java/nl/tue/besportive/Navigator.java
+++ app/src/main/java/nl/tue/besportive/Navigator.java
@@ -5,6 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Objects;
+
 public class Navigator {
     private static final String TAG = "Navigator";
 
@@ -78,6 +86,45 @@ public class Navigator {
     private static void finishActivity(Context context) {
         if (context instanceof Activity) {
             ((Activity) context).finish();
+        }
+    }
+
+
+    public static void navigateToStartingPage(Context context) {
+        Log.i(TAG, "navigateToStartingPage");
+        checkIfUserIsStillLoggedIn(context);
+    }
+
+    private static void checkIfUserIsStillLoggedIn(Context context) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            navigateToOnboardingActivity(context, true);
+            return;
+        }
+
+        user.reload().addOnCompleteListener((task) -> onUserReloaded(context, task));
+    }
+
+    private static void onUserReloaded(Context context, Task<Void> task) {
+        if (task.isSuccessful()) {
+            checkIfUserHasGroup(context, Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()));
+        } else {
+            Navigator.navigateToOnboardingActivity(context, true);
+        }
+    }
+
+    private static void checkIfUserHasGroup(Context context, FirebaseUser user) {
+        FirebaseFirestore.getInstance().collection("groups")
+                .whereNotEqualTo("members." + user.getUid(), null)
+                .get()
+                .addOnCompleteListener((task) -> onGroupQueryComplete(context, task));
+    }
+
+    private static void onGroupQueryComplete(Context context, Task<QuerySnapshot> task) {
+        if (task.isSuccessful() && !task.getResult().isEmpty()) {
+            Navigator.navigateToFeedActivity(context, true);
+        } else {
+            Navigator.navigateToJoinCreateGroupActivity(context, true);
         }
     }
 }
