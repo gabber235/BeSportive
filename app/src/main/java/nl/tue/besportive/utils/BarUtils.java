f9adb26c8561b71b114bbb20e650a8f1a7b5ac8e
Gabber235 <gabber200@icloud.com>
2023-04-05 09:32:59 +0200

    Use profile icon toolbar icon

--- app/src/main/java/nl/tue/besportive/utils/BarUtils.java
+++ app/src/main/java/nl/tue/besportive/utils/BarUtils.java
@@ -2,19 +2,63 @@ package nl.tue.besportive.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import nl.tue.besportive.R;
 
 public class BarUtils {
     public static Toolbar setupPrimaryToolbar(Toolbar toolbar) {
         toolbar.setTitle("");
-        toolbar.setNavigationIcon(R.drawable.img); // Replace back button with profile image.
+
+        toolbar.setNavigationIcon(R.drawable.img); // This is the default icon. Will be replaced by the profile picture.
+
+        Target target = new Target() {
+            @Override
+            public void onBitmapLoaded(android.graphics.Bitmap bitmap, Picasso.LoadedFrom from) {
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, false);
+                toolbar.setNavigationIcon(new BitmapDrawable(toolbar.getResources(), scaledBitmap));
+            }
+
+            @Override
+            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                toolbar.setNavigationIcon(errorDrawable);
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                toolbar.setNavigationIcon(placeHolderDrawable);
+            }
+        };
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            return toolbar;
+        }
+
+        FirebaseFirestore.getInstance().document("users/" + user.getUid()).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                String photoUrl = task.getResult().getString("photoUrl");
+                if (photoUrl != null) {
+                    Picasso.get().load(photoUrl)
+                            .error(R.drawable.img)
+                            .placeholder(R.drawable.img)
+                            .into(target);
+                }
+            }
+        });
 
         return toolbar;
     }
